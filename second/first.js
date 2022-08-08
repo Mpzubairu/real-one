d7dc9c3fd704e31f5cb6ccc93cb2557e202a13e4
musa philip zubairu <zubairuphilipmusa@gmail.com>
2022-08-08 03:22:32 -1200

    change files

---
+++ second/first.js
@@ -0,0 +1,80 @@
+sal({
+    // threshold: 1,
+    once: false
+});
+
+var tabPanel = document.querySelectorAll(".tab-panel")
+var tabButton = document.querySelectorAll(".tab-button")
+var darkModeColor = localStorage.getItem("darkmode")
+var darkModeButton = document.getElementsByClassName('dark-mode')
+var submit =document.querySelector(".submit")
+
+// div transition
+
+ 
+// tab configuration 
+
+function tabbed(position, screen){
+    tabButton.forEach(element => {
+        element.style.backgroundColor =''
+        element.style.color=''
+    });
+    tabButton[position].style.backgroundColor=screen
+    tabButton[position].style.color='white'
+
+    tabPanel.forEach(element=>{
+        element.style.display='none'
+    })
+    tabPanel[position].style.display='block'
+
+}
+tabbed(0, '#161616')
+
+// dark mode configuration
+
+const addDarkmode = ()=>{
+    document.body.classList.add('darkmodecolor')
+    localStorage.setItem("darkmode", "enabled")
+    darkModeButton[0].style.backgroundColor = '#161616'
+    darkModeButton[0].style.color = 'white'
+}
+const removeDarkmode = ()=>{
+    document.body.classList.remove('darkmodecolor')
+    localStorage.setItem("darkmode", null)
+    darkModeButton[0].style.backgroundColor = 'rgb(82, 80, 80)'
+    darkModeButton[0].style.color = '#161616'
+    
+}
+if (darkModeColor =='enabled'){
+    addDarkmode()
+}
+
+function darkMode(){
+    console.log('this is working')
+    var darkModeColor = localStorage.getItem("darkmode")
+    if (darkModeColor !== "enabled" ){
+       addDarkmode()
+        console.log(darkModeColor)
+    }
+    else {
+        removeDarkmode()
+        console.log(darkModeColor)
+    }
+}
+
+// send email section
+var sendmail = function(e) {
+    var form = document.getElementById('contact-form');
+       emailjs.sendForm('Portfolio', 'template_vvpgasx', form)
+           .then(function() {
+               console.log('SUCCESS!');
+               alert('email send SUCCESS');
+           }, function(error) {
+               console.log('FAILED...', error);
+               alert('FAILED EMAIL');
+           });
+
+}
+submit.addEventListener('click', sendmail(e));
+
+
